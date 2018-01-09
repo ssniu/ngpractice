ec3591adf230fbd23a34aa50d2d3e23923c0c279
ssniu <ssniu@ssnius-iMac.local>
2018-01-09 18:45:33 -0500

    rm

--- hackhall-master/routes/index.js
+++
@@ -1,5 +0,0 @@
-exports.posts = require('./posts');
-exports.main = require('./main');
-exports.users = require('./users');
-exports.application = require('./application');
-exports.auth = require('./auth');
